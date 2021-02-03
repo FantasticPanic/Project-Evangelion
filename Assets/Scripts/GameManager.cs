270524f398f9717cc3a71bbc4f65454253b87551
Nicholas Ramsay <nicholasramsay@hotmail.com>
2021-02-03 02:26:16 -0800

    Add player ships, enemy ships and colliders

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraSetup();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CameraSetup()
+    {
+        GameObject gameCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        //Camera Transform
+        gameCamera.transform.position = new Vector3(0, 0, -300);
+        gameCamera.transform.eulerAngles = new Vector3(0, 0, 0);
+
+        //Camera Properties
+        gameCamera.GetComponent<Camera>().clearFlags = CameraClearFlags.SolidColor;
+        gameCamera.GetComponent<Camera>().backgroundColor = new Color32(0, 0, 0, 255);
+
+    }
+}
